f33f1ec1ba17e1dd59687135ad3e1e0c66a940e8
rafalwilk4ti1 <61004695+rafalwilk4ti1@users.noreply.github.com>
2020-02-28 19:08:58 +0100

    Update and rename wersja1.py to SCRIPTCreationFiles.py

---
+++ SCRIPTCreationFiles.py
@@ -0,0 +1,28 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='opis skryptu')
+    parser.add_argument("filename", help="Nazwa tworzonego pliku", type=str)
+    parser.add_argument("name", help="Linia tekstu która ma zostać napisana w pliku", type=str)
+    parser.add_argument("count", help="Liczba linii tekstu", type=int)
+
+    args = parser.parse_args()
+
+    amount = int(args.count)
+    title = str(args.filename)
+    content = args.name
+
+
+    if title:
+        print("Great you called your file!")
+        new = open(title, encoding='utf-8', mode='w')
+        if amount:
+            print("Congratulations, you have chosen how many lines will be in your file")
+            for i in range(amount):
+                if content:
+                    new.write(content+"\n")
+print("Fantastic, you've done")
+
+main()
+
