742c5c8ce36897972ddde866a620585b1d10f80b
rafalwilk4ti1 <61004695+rafalwilk4ti1@users.noreply.github.com>
2020-03-02 12:32:20 +0100

    Update SCRIPTCreationFiles.py

--- SCRIPTCreationFiles.py
+++ SCRIPTCreationFiles.py
@@ -3,9 +3,9 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='opis skryptu')
-    parser.add_argument("--filename", help="Nazwa tworzonego pliku", type=str, dest="filename")
-    parser.add_argument("--name", help="Linia tekstu która ma zostać napisana w pliku", type=str, dest="name")
-    parser.add_argument("--count", help="Liczba linii tekstu", type=int)
+    parser.add_argument("--filename", help="Nazwa tworzonego pliku", type=str, dest="filename", required=True)
+    parser.add_argument("--name", help="Linia tekstu która ma zostać napisana w pliku", type=str, dest="name", required=True)
+    parser.add_argument("--count", help="Liczba linii tekstu", type=int, required=True)
 
     args = parser.parse_args()
 
@@ -20,14 +20,11 @@ def main():
 def make(amount,title,content):
 
 
-    if title:
-        print("Great you called your file!")
-        new = open(title, encoding='utf-8', mode='w')
-        if amount:
-            print("Congratulations, you have chosen how many lines will be in your file")
-            for i in range(amount):
-                if content:
-                    new.write(content+"\n")
+    print("Great you called your file!")
+    new = open(title, encoding='utf-8', mode='w')
+    print("Congratulations, you have chosen how many lines will be in your file")
+    for i in range(amount):
+        new.write(content+"\n")
 print("Fantastic, you've done")
 
 
